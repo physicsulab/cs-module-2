5b130d9caab50c533066fe48b9027380dcf1df62
physicsulab <44276312+physicsulab@users.noreply.github.com>
2019-10-10 05:25:38 -0700

    Trying grader again

--- csModule2.py
+++ csModule2.py
@@ -8,6 +8,19 @@ with open('token.txt') as f:
     token = f.readline()
 # Checkpoints
 
+key = 'hello hello'
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+token = decode(key, token)
+
 
 def checkpoint1(cp1):
     # cp1 function
